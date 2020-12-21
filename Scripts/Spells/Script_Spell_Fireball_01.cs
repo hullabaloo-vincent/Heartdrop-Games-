abe591df7d347aa481c0d78b1d56d643720b9653
hullabaloo-vincent <vincega13@gmail.com>
2020-12-21 16:46:38 -0500

    Fireball now collides with enemies
    Deals damage to enemy class.

--- Scripts/Spells/Script_Spell_Fireball_01.cs
+++ Scripts/Spells/Script_Spell_Fireball_01.cs
@@ -2,32 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Script_Spell_Fireball_01 : MonoBehaviour {
+public class Script_Spell_Fireball_01 : MonoBehaviour
+{
     public float destroy_time = 3f;
     public float damage = 2.5f;
-    
+
     Rigidbody rd;
 
-    void Start() {
+    void Start()
+    {
         rd = gameObject.GetComponent<Rigidbody>();
         Destroy(gameObject, destroy_time);
     }
 
-    void Update() {
+    void Update()
+    {
         rd.AddForce(transform.forward * 10f);
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Enemy") {
-            other.gameObject.GetComponentInParent<Script_Enemy_Base>().recieveDamage(damage);
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            // force is how forcefully we will push the player away from the enemy.
+            float force = 20;
+            other.gameObject.GetComponentInParent<Script_Enemy_Base>().RecieveDamage(damage);
+            Rigidbody enemyRigidBody = other.gameObject.GetComponent<Rigidbody>();
+            enemyRigidBody.MovePosition(enemyRigidBody.position + -other.transform.forward * force * Time.deltaTime);
             Destroy(gameObject);
         }
 
-        if (other.gameObject.tag == "CollidableObstacles"){
+        if (other.gameObject.tag == "CollidableObstacles")
+        {
             Destroy(gameObject);
         }
 
-        if (other.gameObject.tag == "Default"){
+        if (other.gameObject.tag == "Default")
+        {
             Destroy(gameObject);
         }
     }
