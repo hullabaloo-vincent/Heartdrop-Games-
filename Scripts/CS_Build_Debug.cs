db42a88b1d914e7ec5262aad54d35e57a698017a
hullabaloo-vincent <vincega13@gmail.com>
2021-04-23 10:55:29 -0400

    Debug log for runtime

---
+++ Scripts/CS_Build_Debug.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CS_Build_Debug : MonoBehaviour {
+    string myLog = "*begin log";
+    string filename = "";
+    bool doShow = true;
+    int kChars = 700;
+    void OnEnable() { Application.logMessageReceived += Log; }
+    void OnDisable() { Application.logMessageReceived -= Log; }
+    void Update() { if (Input.GetKeyDown(KeyCode.Space)) { doShow = !doShow; } }
+    public void Log(string logString, string stackTrace, LogType type) {
+        // for onscreen...
+        string new_info = "________\n" + logString + "\n" + stackTrace + "\n" + type + "\n";
+        myLog =  myLog + new_info;
+        if (myLog.Length > kChars) { myLog = myLog.Substring(myLog.Length - kChars); }
+
+        // for the file ...
+        if (filename == "") {
+            string d = System.Environment.GetFolderPath(
+               System.Environment.SpecialFolder.Desktop) + "/YOUR_LOGS";
+            System.IO.Directory.CreateDirectory(d);
+            string r = Random.Range(1000, 9999).ToString();
+            filename = d + "/log-" + r + ".txt";
+        }
+        try { System.IO.File.AppendAllText(filename, new_info + "\n"); } catch { }
+    }
+
+    void OnGUI() {
+        if (!doShow) { return; }
+        GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity,
+           new Vector3(Screen.width / 1200.0f, Screen.height / 800.0f, 1.0f));
+        GUI.TextArea(new Rect(10, 10, 540, 370), myLog);
+    }
+}
+
+
