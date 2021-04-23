20af5fb5e49fcc297fe9f576ca05adcf20432932
hullabaloo-vincent <vincega13@gmail.com>
2021-04-23 10:54:42 -0400

    Player footsteps

---
+++ Scripts/Player/Script_Player_Sound_Manager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Player_Sound_Manager : MonoBehaviour
+{
+    public void PlayFootstep() {
+        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Footsteps/Footsteps_Concrete", gameObject);
+    }
+}
