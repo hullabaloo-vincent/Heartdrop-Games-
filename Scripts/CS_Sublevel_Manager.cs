78f050759be1b689037c7f0c8745f8ec4b8e94b1
hullabaloo-vincent <vincega13@gmail.com>
2021-04-23 10:52:46 -0400

    Sublevel scripts

---
+++ Scripts/CS_Sublevel_Manager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CS_Sublevel_Manager : MonoBehaviour {
+    void Start() {
+        // Initializes the value of _HasBeenTriggered on game start
+        _HasBeenTriggered = false;
+    }
+    //
+    // Summary:
+    //      Preforms the level loading based on the player entering a trigger area
+     void OnTriggerEnter(Collider other)
+    {
+        // If the collided object is the player and the box hasn't been triggered before.
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log(LoadScenes.Length);
+            if (LoadScenes.Length > 0) {
+                foreach (string scenes in LoadScenes) {
+                    if (!SubLevelMaster.GetLoadedScenes().Contains(scenes)) {
+                        SubLevelMaster.AddLoadedScene(scenes);
+                        StartCoroutine(LoadSceneAsync(LoadSceneMode.Additive, scenes));
+                    }
+                }
+            }
+            if (UnloadScenes.Length > 0)
+            {
+                // Asynchronously unloads the selected scenes.
+                foreach (string scenes in UnloadScenes) {
+                    if (SubLevelMaster.GetLoadedScenes().Contains(scenes)) {
+                        SubLevelMaster.RemoveLoadedScene(scenes);
+                        SceneManager.UnloadSceneAsync(scenes);
+                    }
+                }
+            }
+        }
+    }
+
+    //
+    // Summary:
+    //      Loads a scene asynconously. It will load a scene only once the scene fully loads.
+    //
+    // Parameters:
+    //   lsm:
+    //     The enum value of LoadSceneMode. Either LoadSceneMode.Additive or LoadSceneMode.Single.
+    //
+    // Returns:
+    //     null
+    IEnumerator LoadSceneAsync(LoadSceneMode lsm, string scene_to_load) {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene_to_load, lsm);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+    }
+
+
+    //
+    // Summary:
+    //      This enum holds the values of the scenes to load
+    public string[] LoadScenes;
+    //
+    // Summary:
+    //      This enum holds the values of the scenes to unload
+    public string[] UnloadScenes;
+    //
+    // Summary:
+    //      Ensures that once that loading/unloading the scene can happen only once.
+    private bool _HasBeenTriggered;
+
+    public CS_Sublevel_Master SubLevelMaster;
+ 
+}
---
+++ Scripts/CS_Sublevel_Master.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Sublevel_Master : MonoBehaviour
+{
+    public List<string> LoadedScenes;
+
+    public void Start() {
+        LoadedScenes = new List<string>();
+    }
+
+    public void AddLoadedScene(string new_scene) {
+        LoadedScenes.Add(new_scene);
+    }
+    public void RemoveLoadedScene(string old_scene) {
+        LoadedScenes.Remove(old_scene);
+    }
+    public List<string> GetLoadedScenes() {
+        return LoadedScenes;
+    }
+}
